38fa0c083afd7659a591c85379e63cdcebdd89cb
Ahmed Sengab <sengab87@gmail.com>
2018-12-18 22:36:45 -0500

    fifth lesson

--- WindowShopper/View/CurrencyTxtField.swift
+++ WindowShopper/View/CurrencyTxtField.swift
@@ -26,6 +26,7 @@ class CurrencyTxtField: UITextField {
         self.textAlignment = .center
         self.keyboardType = .decimalPad
         self.borderStyle = .none
+        self.clipsToBounds = true
         if (placeholder == nil)
         {
             placeholder = " "
@@ -33,6 +34,22 @@ class CurrencyTxtField: UITextField {
         let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: UIColor.white])
         attributedPlaceholder = place
         textColor = UIColor.white
+    }
+    override func draw(_ rect: CGRect) {
+        // Never implement it when it is empty
+        // Never call super
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: frame.size.height/2 - size/2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7631019354, green: 0.7574522495, blue: 0.7674265504, alpha: 1)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.4352941176, green: 0.4431372549, blue: 0.4745098039, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
     }
 
 }
