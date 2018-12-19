9ca939171b3617251c32e0d0da2fb6b28257a4ee
Ahmed Sengab <sengab87@gmail.com>
2018-12-18 20:29:26 -0500

    second push

--- WindowShopper/Controller/MainVC.swift
+++ WindowShopper/Controller/MainVC.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt : CurrencyTxtField!
+    @IBOutlet weak var priceTxt : CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupButton()
+       
+    }
+    @objc func calculate(){
+    
+        print("yo")
+    }
+    func setupButton(){
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.4720282555, blue: 0.2901404202, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        /// CalcBtn.txt dont work
     }
 
 
